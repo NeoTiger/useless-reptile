25c1c205031b69acf7d5913244e636386bae1656
Nord_Act <73118477+NordAct@users.noreply.github.com>
2025-02-25 11:39:29 +0300

    Fix dyn registries not working

--- src/main/java/nordmods/uselessreptile/common/init/URRegistryKeys.java
+++ src/main/java/nordmods/uselessreptile/common/init/URRegistryKeys.java
@@ -3,7 +3,7 @@ package nordmods.uselessreptile.common.init;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
-import nordmods.uselessreptile.UselessReptile;
+import net.minecraft.util.Identifier;
 import nordmods.uselessreptile.common.util.dragon_variant.DragonVariant;
 import nordmods.uselessreptile.common.util.dragon_variant.model.DragonEquipment;
 import nordmods.uselessreptile.common.util.dragon_variant.model.DragonModel;
@@ -12,17 +12,17 @@ import nordmods.uselessreptile.common.util.dragon_variant.spawn.DragonSpawnCondi
 import java.util.List;
 
 public class URRegistryKeys {
-    public static final RegistryKey<Registry<DragonVariant>> DRAGON_VARIANT = RegistryKey.ofRegistry(UselessReptile.id("dragon_variant/variant"));
-    public static final RegistryKey<Registry<DragonVariant>> DRAGON_VARIANT_CUSTOM_NAME = RegistryKey.ofRegistry(UselessReptile.id("dragon_variant/custom_name"));
-    public static final RegistryKey<Registry<DragonModel>> DRAGON_MODEL = RegistryKey.ofRegistry(UselessReptile.id("dragon_variant/dragon_model"));
-    public static final RegistryKey<Registry<List<DragonEquipment>>> DRAGON_EQUIPMENT = RegistryKey.ofRegistry(UselessReptile.id("dragon_variant/equipment"));
-    public static final RegistryKey<Registry<List<DragonSpawnConditions>>> DRAGON_SPAWN_CONDITIONS = RegistryKey.ofRegistry(UselessReptile.id("dragon_variant/spawn_conditions"));
+    public static final RegistryKey<Registry<DragonVariant>> DRAGON_VARIANT = RegistryKey.ofRegistry(Identifier.of("dragon_variant","variant"));
+    public static final RegistryKey<Registry<DragonVariant>> DRAGON_VARIANT_CUSTOM_NAME = RegistryKey.ofRegistry(Identifier.of("dragon_variant","custom_name"));
+    public static final RegistryKey<Registry<DragonModel>> DRAGON_MODEL = RegistryKey.ofRegistry(Identifier.of("dragon_variant","dragon_model"));
+    public static final RegistryKey<Registry<List<DragonEquipment>>> DRAGON_EQUIPMENT = RegistryKey.ofRegistry(Identifier.of("dragon_variant","equipment"));
+    public static final RegistryKey<Registry<List<DragonSpawnConditions>>> DRAGON_SPAWN_CONDITIONS = RegistryKey.ofRegistry(Identifier.of("dragon_variant","spawn_conditions"));
 
     public static void init() {
-        DynamicRegistries.registerSynced(DRAGON_VARIANT, DragonVariant.CODEC, DragonVariant.CODEC_NO_SPAWN_INFO, DynamicRegistries.SyncOption.SKIP_WHEN_EMPTY);
-        DynamicRegistries.registerSynced(DRAGON_VARIANT_CUSTOM_NAME, DragonVariant.CODEC_NO_SPAWN_INFO, DragonVariant.CODEC_NO_SPAWN_INFO, DynamicRegistries.SyncOption.SKIP_WHEN_EMPTY);
-        DynamicRegistries.registerSynced(DRAGON_MODEL, DragonModel.CODEC, DynamicRegistries.SyncOption.SKIP_WHEN_EMPTY);
-        DynamicRegistries.registerSynced(DRAGON_EQUIPMENT, DragonEquipment.CODEC.listOf(), DynamicRegistries.SyncOption.SKIP_WHEN_EMPTY);
+        DynamicRegistries.registerSynced(DRAGON_VARIANT, DragonVariant.CODEC, DragonVariant.CODEC_NO_SPAWN_INFO);
+        DynamicRegistries.registerSynced(DRAGON_VARIANT_CUSTOM_NAME, DragonVariant.CODEC_NO_SPAWN_INFO);
+        DynamicRegistries.registerSynced(DRAGON_MODEL, DragonModel.CODEC);
+        DynamicRegistries.registerSynced(DRAGON_EQUIPMENT, DragonEquipment.CODEC.listOf());
         DynamicRegistries.register(DRAGON_SPAWN_CONDITIONS, DragonSpawnConditions.CODEC.listOf());
     }
 }
